b05dc628bacdd7d0dfac4bea1a83522f11de6b3b
Nimesh Nikum <nimesh.nikum@gmail.com>
2020-04-16 02:12:13 -0500

    Implement category and ratings facets filters in movies listing page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,5 +3,22 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    @categories = Category.includes(:movies).select { |c| c.movies.count > 0 }
+    @group_by_ratings = @movies.group_by(&:average_rating)
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,12 @@
 class Movie < ApplicationRecord
   has_and_belongs_to_many :categories
   belongs_to :user, optional: true
+
+  has_many :ratings
+
+  def average_rating
+    valid_ratings = ratings.reject { |r| r.rating.nil? }
+    return nil if valid_ratings.blank?
+    valid_ratings.sum(0) / valid_ratings.size
+  end
 end
